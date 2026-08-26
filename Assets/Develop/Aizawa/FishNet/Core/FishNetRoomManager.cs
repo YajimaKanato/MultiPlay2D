c161b5a7052177f406caa6b54641e459883aec2f
AizawaYuki <imagination1159@gmail.com>
2026-08-26 19:25:19 +0900

    [add]FishNetを使用した部屋立てを作成

--- Assets/Develop/Aizawa/FishNet/Core/FishNetRoomManager.cs
+++ Assets/Develop/Aizawa/FishNet/Core/FishNetRoomManager.cs
@@ -1,16 +1,72 @@
+using System;
+using FishNet;
+using PlayEveryWare.EpicOnlineServices;
 using UnityEngine;
 
+/// <summary>
+/// FishNetによるネットワーク接続を管理するコンポーネント
+/// </summary>
 public class FishNetRoomManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        if(FindObjectsByType<FishNetRoomManager>(FindObjectsSortMode.None).Length == 1)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// ホストとして部屋を立てる
+    /// </summary>
+    public void StartRoom()
     {
-        
+        Login(() =>
+        {
+            InstanceFinder.ServerManager.StartConnection();
+            InstanceFinder.ClientManager.StartConnection();
+
+            var eosManager = EOSManager.Instance;
+            if (eosManager != null)
+            {
+                string puid = eosManager.GetProductUserId().ToString();
+                GameDebug.Log($"PUID: {puid}");
+            }
+        });
+    }
+
+    /// <summary>
+    /// EOSにログインする
+    /// </summary>
+    /// <param name="whenLogined">ログイン成功後の処理</param>
+    private void Login(Action whenLogined)
+    {
+        var eosManager = EOSManager.Instance;
+        if(eosManager.GetProductUserId() != null)
+        {
+            whenLogined();
+        }
+        else
+        {
+            eosManager.StartConnectLoginWithDeviceToken(
+                $"Player_{UnityEngine.Random.Range(1000, 9999)}",
+                loginInfo =>
+                {
+                    if (loginInfo.ResultCode == Epic.OnlineServices.Result.Success)
+                    {
+                        Debug.Log("ログインに成功しました。");
+                        whenLogined();
+                    }
+                    else
+                    {
+                        Debug.LogError($"ログインに失敗しました: {loginInfo.ResultCode}");
+                    }
+                }
+            );
+        }
     }
 }
